8b6df6c6d504dd8882dc6398e2b70b9a46eba534
georgiev098 <geor0014@hz.nl>
2022-12-05 13:33:31 +0100

    add waitress prod server

--- python-processing/app.py
+++ python-processing/app.py
@@ -1,25 +1,15 @@
-import time
-import redis
 from flask import Flask
+from waitress import serve  
 from processImg import process_food
 
 # import the welcome function from welcome.py
 app = Flask(__name__)
-cache = redis.Redis(host='redis', port=6379)
-
-# def get_hit_count():
-#     retries = 5
-#     while True:
-#         try:
-#             return cache.incr('hits')
-#         except redis.exceptions.ConnectionError as exc:
-#             if retries == 0:
-#                 raise exc
-#             retries -= 1
-#             time.sleep(0.5)
 
 @app.route('/')
 def hello( ):
     # display values from process_food function as a string
     return str(process_food())
-    
+
+if __name__ == '__main__':
+    # run the app.
+    serve(app, host='0.0.0.0', port=5000)
