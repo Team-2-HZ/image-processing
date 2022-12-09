03c3d60a9eab03deb3a155fba2df26b8f2f1c1da
georgiev098 <geor0014@hz.nl>
2022-12-09 15:15:08 +0100

    update post request to work with VPS and store to db

--- python-processing/receiveImg.py
+++ python-processing/receiveImg.py
@@ -1,6 +1,10 @@
 from flask import request, jsonify
 from processImg import process_food_from_image
 from PIL import Image
+import requests
+
+bearerTkn = 'miTQ1NwbocCI?A2uyop1?VN=l3wh?kebR6WuepYJCOFfzWqGImXfiO/Ksed5pAxQBP8km8qU!6RmhehCPlF5D7TZm?R8w4bH8JpQXxrgABVDfAHyC9yBp3M2zxCQN13-oSf-fJhqjY-X9HlyMyq6y3Rm486eOx5VGWt!upDx-Y3CorzLs747otpnGEcfOQozNoSzJqlC!PZGypR22j/2DD1jzuCml!eHjfkX=sT8lQYqabuOnAJ/fhI6HKdo1p0X'
+apiUrl = 'http://nutri.larskra.de/api/v1/nutrition'
 
 
 def process_image_from_url():
@@ -14,5 +18,12 @@ def process_image_from_url():
     # process the image with the pre-trained model
     img = process_food_from_image()
     # return the processed image
-    data = {'food': img[0], 'grams': weight}
-    return data
+    data = {'food': img[0], 'grams': int(weight)}
+
+    # make a post to the API
+    response = requests.post(
+        apiUrl,
+        json={"data": data},
+        headers={"Authorization": f"Bearer {bearerTkn}"})
+
+    return response.json()
